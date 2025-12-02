1f09311e7d51488c3b93cdad41cdfe815b724f50
Giorgio <giorgio.bono@student-cs.fr>
2025-12-03 00:05:01 +0100

    Lab 03 - start implementing operators files to obtain the final formula blocks

--- Lab03-blocks/blkOperators.cpp
+++ Lab03-blocks/blkOperators.cpp
@@ -1,1 +1,37 @@
-// To be done
+#include "blkOperators.hpp"
+
+blk::expr blk::operator/(blk::expr a, blk::expr b) {
+  return blk::over(a, blk::over(blk::text("-"), b));
+}
+
+blk::expr blk::operator+(blk::expr a, blk::expr b) {
+  return blk::beside(a, blk::beside(blk::text(" + "), b));
+}
+
+blk::expr blk::operator==(blk::expr a, blk::expr b) {
+  return blk::beside(a, blk::beside(blk::text(" = "), b));
+}
+
+blk::expr blk::operator/(double a, blk::expr b) {
+  return blk::over(blk::text("3"), blk::over(blk::text("-"), b));
+}
+
+blk::expr blk::operator+(double a, blk::expr b) {
+  return blk::beside(blk::text("3"), blk::beside(blk::text(" + "), b));
+}
+
+blk::expr blk::operator==(double a, blk::expr b) {
+  return blk::beside(blk::text("3"), blk::beside(blk::text(" = "), b));
+}
+
+blk::expr blk::operator/(blk::expr a, double b) {
+  return blk::over(a, blk::over(blk::text("-"), blk::text("5")));
+}
+
+blk::expr blk::operator+(blk::expr a, double b) {
+  return blk::beside(a, blk::beside(blk::text(" + "), blk::text("5")));
+}
+
+blk::expr blk::operator==(blk::expr a, double b) {
+  return blk::beside(a, blk::beside(blk::text(" = "), blk::text("5")));
+}
--- Lab03-blocks/blkOperators.hpp
+++ Lab03-blocks/blkOperators.hpp
@@ -1,4 +1,21 @@
 #pragma once
 
-// To be done
+#include <string>
+#include <blkBlock.hpp>
+#include <blkOver.hpp>
+#include <blkBeside.hpp>
+#include <blkText.hpp>
 
+namespace blk {
+  expr operator/(expr a, expr b);
+  expr operator+(expr a, expr b);
+  expr operator==(expr a, expr b);
+
+  expr operator/(double a, expr b);
+  expr operator+(double a, expr b);
+  expr operator==(double a, expr b);
+
+  expr operator/(expr a, double b);
+  expr operator+(expr a, double b);
+  expr operator==(expr a, double b);
+}
