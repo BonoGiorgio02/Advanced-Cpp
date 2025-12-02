346913f77f0b3b1561453a9329db27c7bd2bc49a
BonoGiorgio02 <bono.giorgio.02@gmail.com>
2025-12-03 00:25:49 +0100

    Lab 03 - improving fractions layout in operators file

--- Lab03-blocks/blkOperators.cpp
+++ Lab03-blocks/blkOperators.cpp
@@ -1,7 +1,7 @@
 #include "blkOperators.hpp"
 
 blk::expr blk::operator/(blk::expr a, blk::expr b) {
-  return blk::over(a, blk::over(blk::text("-"), b));
+  return blk::over(a, blk::over(blk::text(std::string(std::max(a->get_width(), b->get_width()) + 2, '-')), b));
 }
 
 blk::expr blk::operator+(blk::expr a, blk::expr b) {
@@ -13,7 +13,7 @@ blk::expr blk::operator==(blk::expr a, blk::expr b) {
 }
 
 blk::expr blk::operator/(double a, blk::expr b) {
-  return blk::over(blk::text("3"), blk::over(blk::text("-"), b));
+  return blk::over(blk::text("3"), blk::over(blk::text(std::string(5, '-')), b));
 }
 
 blk::expr blk::operator+(double a, blk::expr b) {
@@ -25,7 +25,7 @@ blk::expr blk::operator==(double a, blk::expr b) {
 }
 
 blk::expr blk::operator/(blk::expr a, double b) {
-  return blk::over(a, blk::over(blk::text("-"), blk::text("5")));
+  return blk::over(a, blk::over(blk::text(std::string(5, '-')), blk::text("5")));
 }
 
 blk::expr blk::operator+(blk::expr a, double b) {
