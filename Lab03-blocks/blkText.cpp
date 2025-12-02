58ea6bee189c45048acdb19bbadedfb834f59204
Giorgio <giorgio.bono@student-cs.fr>
2025-12-02 22:35:16 +0100

    Lab 03 - added possibility to create Text blocks

--- Lab03-blocks/blkText.cpp
+++ Lab03-blocks/blkText.cpp
@@ -1,1 +1,29 @@
-// To be done
+#include "blkText.hpp"
+
+blk::expr blk::text(const std::string& txt, align al) {
+  return std::make_shared<Text>(txt, al);
+}
+
+int blk::Text::compute_width(std::string txt) {
+  return txt.length();
+}
+
+int blk::Text::compute_ref_width(std::string txt, align al) {
+  if(al == align::center)
+    return txt.length()/2;
+  else if(al == align::left)
+    return 0;
+  else
+    return txt.length() - 1;
+}
+
+blk::Text::Text(const std::string& txt, align al) 
+                : Block(blk::Text::compute_width(txt), 1, 
+                        blk::Text::compute_ref_width(txt, al), 0),
+                  txt(txt), al(al) {}
+
+blk::Text::~Text() {}
+
+void blk::Text::print_inbounds_line(std::ostream& os, int line) const {
+  os << txt;
+}
--- Lab03-blocks/blkText.hpp
+++ Lab03-blocks/blkText.hpp
@@ -1,4 +1,24 @@
 #pragma once
 
-// To be done
+#include <string>
+#include <blkBlock.hpp>
 
+namespace blk {
+  enum class align : char {left, center, right};
+
+  expr text(const std::string& txt, align al = align::center);
+
+  class Text : public Block{
+    private:
+      std::string txt;
+      align al;
+
+      static int compute_width(std::string txt);
+      static int compute_ref_width(std::string txt, align al);
+
+    public:
+      Text(const std::string& txt, align al);
+      virtual ~Text();
+      virtual void print_inbounds_line(std::ostream& os, int line) const override;
+  };
+}
